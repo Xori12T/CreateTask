e7e88a226bc6d5369f921de178248fcff79f7306
Xori12T <mistermaster1013@gmail.com>
2026-04-27 10:17:26 -0700

    Added comments
    For create task

--- cardmatchcreatetask.py
+++ cardmatchcreatetask.py
@@ -1,13 +1,18 @@
+#imports modules
 import turtle
 import random
 
+#initializes game interface
 screen = turtle.Screen()
 
+#changes screen size to match with device's screen size
 displaywidth = screen.cv.winfo_screenwidth()
 displayheight = screen.cv.winfo_screenheight()
 screen.setup(displaywidth*0.45,displayheight*0.9)
 
+#more screen configuration
 screen.bgcolor("#B9CBD9")
+#initializes extra turtles for game interface
 writer = turtle.Turtle()
 alert = turtle.Turtle()
 writer.hideturtle()
@@ -17,17 +22,22 @@ alert.pensize(20)
 writer.up()
 alert.up()
 writer.speed(0)
+#writer turtle writes game instructions
 writer.goto(0,270)
 writer.write("Match!", align="center", font=("Arial", 100, "normal"))
 writer.goto(0,-390)
 writer.write("Click on each card to reveal its color.\nMatch all cards in the least turns possible!", align="center", font=("Arial", 25, "normal"))
+
+#makes and shuffles the color list for the cards
 collist = ["cyan", "blue", "green", "red", "yellow", "purple", "orange", "chartreuse"]
 random.shuffle(collist)
+#positions that each card goes to
 poslist = [(-225, 200), (-75, 200), (75, 200), (225,200),
            (-225, 50), (-75, 50), (75, 50), (225, 50),
            (-225, -100), (-75, -100), (75, -100), (225,-100),
            (-225, -250), (-75, -250), (75, -250), (225,-250)]
 
+#initializes variables
 first = None
 second = None
 firstcard = None
@@ -36,6 +46,7 @@ locked = False
 count = 1
 turns = 0
 
+#user defined function for pop up text
 def notice(txt, align=35):
     fs = 50
     alert.up()
@@ -57,14 +68,15 @@ def notice(txt, align=35):
     alert.write(txt, align="center", font=("Arial", fs, "normal"))
     screen.ontimer(noterase, 800)
 
-
+#very small function to clear the pop up after a time with ontimer and doesn't stop the program's runtime
 def noterase():
     alert.clear()
     
-
+#small function to compare two clicked cards
 def check(f, s):
     return f == s
 
+#more actually ran code, asks for difficulty at start of the game
 difficulty = screen.textinput("Difficulty", "1 for easy, 2 for normal, 3 for hard, default to easy.")
 if difficulty == "3":
     difficulty = 16
@@ -73,6 +85,8 @@ elif difficulty == "2":
 else:
     difficulty = 8
 
+#real mechanics of the game
+#keeps track of what card was clicked, checks, basically runs the game
 def on_card_click(card, color):
     global count, first, second, turns, firstcard, secondcard, locked
     if not locked:
@@ -86,6 +100,7 @@ def on_card_click(card, color):
                 print("Card clicked: " + color + ". ")
                 card.color("black", color)
             else:
+                #checks if the second card isn't the first card
                 if card != firstcard:
                     locked = True
                     second = color
@@ -93,6 +108,7 @@ def on_card_click(card, color):
                     count = 1
                     print("Card clicked: " + color + ". ")
                     card.color("black", color)
+                    #compares the cards
                     if check(first, second):
                         print("Thats a match!")
                         firstcard.solved = True
@@ -104,6 +120,7 @@ def on_card_click(card, color):
                         notice("That's not\na match...", 75)
                         screen.ontimer(reset, 1000)
 
+#more setup function, creates the list of colors for the cards
 def generate_cards(l, d):
     l2 = l[:]
     nl = []
@@ -117,6 +134,7 @@ def generate_cards(l, d):
         random.shuffle(nl)
     return nl
 
+#debug function
 def showall():
     global cards, turns
     turns += 100
@@ -124,6 +142,7 @@ def showall():
         x.fillcolor(x.revealcolor)
         x.solved = True
 
+#function that runs after each round to make sure all non-solved cards are reset, and checks for win condition
 def reset():
     global cards, firstcard, secondcard, turns, difficulty, locked
     firstcard = None
@@ -149,7 +168,7 @@ def reset():
         print(f"It took you {turns} turns to win in {diff} mode!")
         turtle.bye()
     
-
+#checks win condition, iterates through list
 def donecheck():
     global cards
     isdone = True
@@ -158,23 +177,30 @@ def donecheck():
             isdone = False
     return isdone
 
+#calls generate_cards function to setup game
 cardcollist = generate_cards(collist, difficulty)
 
+#set up cards, each card is an individual turtle
 cards = []
 for x in range(difficulty):
     cd = turtle.Turtle()
     cd.shape("square")
     cd.shapesize(5, 4)
     cd.color("black", "white")
+    #revealcolor is a custom attribute that stores its color
     cd.revealcolor = cardcollist[x]
     cd.solved = False
     cd.speed(0)
     cd.up()
     cd.goto(poslist[x][0], poslist[x][1])
+    #sets up what each turtle does when clicked, runs the game
     cd.onclick(lambda x, y, c=cd, col = cd.revealcolor: on_card_click(c, col))
     cards.append(cd)
 
+#debug keyboard shortcuts
 screen.onkeypress(showall, "s")
 screen.onkeypress(reset, "r")
 screen.listen()
+
+#runs the tk mainloop
 turtle.mainloop()
